9a19f7c81bb378a56d4612ebbea618f077ba3fdb
Rakesh Hemwani <rakeshhemwani654@gmail.com>
2024-12-28 06:13:22 +0530

    Added Entities and STructure

---
+++ src/main/java/com/whitewolfs/rakesh/project/uber/entities/Rider.java
@@ -0,0 +1,18 @@
+package com.whitewolfs.rakesh.project.uber.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Rider {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+    private Double rating;
+}
